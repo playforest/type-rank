cc6f477f4c4ebb761998960d25b70ffcf52767eb
playforest <playforest0x@gmail.com>
2023-11-26 22:10:41 +1100

    dev: toy flask socket server

---
+++ server/dev/flask/static/app.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var socket = io.connect("http://127.0.0.1:5000");
+
+    socket.on('connect', function() {
+        socket.send("User connected!");
+    });
+
+    socket.on('message', function(data) {
+        var messages = document.getElementById('messages');
+        var newMessage = document.createElement('p');
+        newMessage.textContent = data;
+        messages.appendChild(newMessage);
+    });
+
+    var sendBtn = document.getElementById('sendBtn');
+    sendBtn.addEventListener('click', function() {
+
+        var username = document.getElementById('username').value;
+        var message = document.getElementById('message').value;
+
+        console.log(`message to send: ${message}`)
+
+        if (message.length > 0) {
+                socket.send(username + ': ' + message);
+        document.getElementById('message').value = '';
+        }
+
+    });
+});
