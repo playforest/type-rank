8fd449a0b6acd50339d34be65bdedf5d23fb4845
playforest <playforest0x@gmail.com>
2023-11-27 19:51:08 +1100

    fix: progress bar now tracks multiline passages correctly

--- client/src/components/ProgressBar.tsx
+++ client/src/components/ProgressBar.tsx
@@ -8,17 +8,19 @@ interface ProgressBarProps {
 export function ProgressBar({ cursor, cursorRef }: ProgressBarProps) {
     const progressRef = useRef<HTMLDivElement>(null)
     const [lineTrackerWidth, setLineTrackerWidth] = useState<number>(0)
-    const [lineTrackerPos, setLineTrackerPos] = useState<number>(0)
+    const [lineTrackerPosX, setLineTrackerPosX] = useState<number>(0)
+    const [lineTrackerPosY, setLineTrackerPosY] = useState<number>(0)
 
     useEffect(() => {
         if (progressRef.current) {
+            const cursorHeight = cursorRef.current?.offsetHeight ?? 0;
             const newWidth = cursorRef.current?.offsetWidth ?? 0;
-            const newPos = cursorRef.current?.offsetLeft ?? 0;
+            const newPosX = cursorRef.current?.offsetLeft ?? 0;
+            const newPosY = cursorRef.current?.offsetTop ?? 0;
+
             setLineTrackerWidth(newWidth);
-            setLineTrackerPos(newPos);
-
-            console.log('width vals: ', cursorRef.current?.offsetLeft, cursorRef.current?.offsetWidth)
-
+            setLineTrackerPosX(newPosX);
+            setLineTrackerPosY(newPosY + cursorHeight);
         }
     }, [cursor])
 
@@ -26,11 +28,12 @@ export function ProgressBar({ cursor, cursorRef }: ProgressBarProps) {
         <div className='progress' ref={progressRef} style={{
             position: 'absolute', // Position absolutely within the inner container
             bottom: 2, // Position at the bottom of the inner container
-            left: lineTrackerPos,
+            left: lineTrackerPosX,
             width: lineTrackerWidth,
+            top: lineTrackerPosY,
             right: 0,
             height: '3px',
-            backgroundColor: 'black',
+            backgroundColor: '#73dd63',
             borderRadius: '2px',
             transition: 'left 0.2s ease, width 0.3s ease'
         }}></div>
