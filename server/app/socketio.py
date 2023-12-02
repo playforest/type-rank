9d74e71b8f11022e00fc9f3ec107092b29733c96
playforest <playforest0x@gmail.com>
2023-12-02 23:22:05 +1100

    custom connection event for testing

--- server/app/socketio.py
+++ server/app/socketio.py
@@ -5,27 +5,31 @@ from config import Config
 socketio = SocketIO(cors_allowed_origins=Config.SOCKETIO_CORS_ALLOWED_ORIGINS)
 
 @socketio.on('connect')
-def on_connect():
+def handle_connect():
     room_id = generate_room_id()
     join_room(room_id)
     emit('room_assigned', {'room_id': room_id})
     print(f'user connected! room: {room_id}')
 
+@socketio.on('custom_connect_event')
+def handle_custom_connect_event(data):
+    print(f'{data}')
+
 @socketio.on('join')
-def on_join(data):
+def handle_join(data):
     room_id = data['room_id']
     join_room(room_id)
     send(f'User has joined the room{room_id}', to=room_id)
     print(f'User has joined room: {room_id}')
 
 @socketio.on('keystroke')
-def on_keystroke(data):
+def handle_keystroke(data):
     print(f'keystroke: {data}')
     send(data, broadcast=True)
 
 
 @socketio.on('leave')
-def on_leave(data):
+def handle_leave(data):
     username = data['username']
     room_id = data['room_id']
     leave_room(room_id)
