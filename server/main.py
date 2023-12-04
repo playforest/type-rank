33e16914b582fae6990e31b653e73338cd230f82
playforest <playforest0x@gmail.com>
2023-12-04 17:04:26 +1100

    remove server from repo

--- server/main.py
+++
@@ -1,7 +0,0 @@
-from app import create_app, socketio
-from config import Config
-
-app = create_app()
-
-if __name__ == '__main__':
-    socketio.run(app, host=Config.FLASK_HOST, port=Config.FLASK_PORT, debug=True)
