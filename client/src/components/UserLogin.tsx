1308f52be376101536cc7154186732fbb90d0223
playforest <playforest0x@gmail.com>
2023-12-14 21:40:49 +1100

    auth: live validation with /usernameExists

--- client/src/components/UserLogin.tsx
+++ client/src/components/UserLogin.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import './UserLoginCss.css'
 
 interface LoginProps {
@@ -13,6 +13,11 @@ export function UserLogin({ onLogin, onRegister }: LoginProps) {
     const [email, setEmail] = useState<string>("")
     const [username, setUsername] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const [timer, setTimer] = useState<number>();
+    const [usernameIsValid, setUsernameIsValid] = useState<boolean | undefined>(undefined);
+
+    const MIN_USERNAME_LENGTH: number = 3;
+    const USERNAME_VERIFICATION_INTERVAL: number = 1000;
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -27,6 +32,56 @@ export function UserLogin({ onLogin, onRegister }: LoginProps) {
         setIsRegistering(!isRegistering);
     };
 
+    const handleOnUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const inputUsername: string = event.target.value;
+        setUsername(inputUsername)
+
+        if (timer) {
+            clearTimeout(timer);
+        }
+
+        const newTimer = setTimeout(() => {
+            if (inputUsername.length >= MIN_USERNAME_LENGTH && isRegistering) {
+                const protocol: string = window.location.protocol;
+                const hostname: string = window.location.hostname;
+                const port: string = window.location.port ? `:${window.location.port}` : '';
+                const usernameExistsUrl = `${protocol}//${hostname}${port}/usernameExists`;
+
+                const options = {
+                    'method': 'POST',
+                    'body': JSON.stringify({
+                        'username': inputUsername
+                    }),
+                    'headers': {
+                        'Content-Type': 'application/json'
+                    }
+                }
+
+                fetch(usernameExistsUrl, options)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data)
+                        if (data.usernameExists) {
+                            setUsernameIsValid(false)
+                        } else {
+                            setUsernameIsValid(true)
+                        }
+                    })
+                console.log(inputUsername)
+            }
+        }, USERNAME_VERIFICATION_INTERVAL);
+
+        setTimer(newTimer);
+    }
+
+    useEffect(() => {
+        return () => {
+            if (timer) {
+                clearTimeout(timer);
+            }
+        }
+    }, [timer])
+
     return (
         <form className={isRegistering ? "user-login is-registering" : "user-login"} onSubmit={handleSubmit}>
             <div className="forms-container">
@@ -39,10 +94,16 @@ export function UserLogin({ onLogin, onRegister }: LoginProps) {
                     />
                 )}
                 <input
+                    className={
+                        isRegistering ?
+                            usernameIsValid === undefined || username.length < MIN_USERNAME_LENGTH
+                                ? 'neutral'
+                                : usernameIsValid ? 'valid' : 'invalid'
+                            : ''}
                     type="text"
                     name="username"
                     value={username}
-                    onChange={(e) => setUsername(e.target.value)}
+                    onChange={handleOnUsernameChange}
                     placeholder="Username"
                 />
                 <input
